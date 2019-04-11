afb730ab36d4dd3dda4700bcea10583c606adda5
eamon <eamon_chen@outlook.com>
2019-04-11 13:38:18 +0800

    spring boot project starter

---
+++ src/main/java/com/eamon/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.eamon.demo.controller;
+ 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class HomeController {
+ 
+    @RequestMapping("/test")
+    public String home(){
+        return "Hello Visual Studio Code!";
+    }
+}
---
+++ src/main/java/com/eamon/demo/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.eamon.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import java.util.HashMap;
+
+@Controller
+public class TestController {
+
+    /**
+     * 本地访问内容地址 ：http://localhost:8080/hello
+     * @param map
+     * @return
+     */
+    @RequestMapping("/hello")
+    public String helloHtml(HashMap<String, Object> map) {
+        map.put("hello", "欢迎进入HTML页面");
+        return "/index";
+    }
+}
